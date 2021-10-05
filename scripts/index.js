653ee42690f8875927fd7b2f5b2030ffc850d9de
rhuanbello <rhuanbello@gmail.com>
2021-10-05 20:02:24 -0300

    Add Input show and Menu show

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,55 @@
 // HEADER TRANSPARENT AT MOVEMENT
 const showButton = () => window.scrollY >= 100 ? document.querySelector('header.fixed').classList.add('movement') : document.querySelector('header.fixed').classList.remove('movement')
 window.addEventListener('scroll', () => showButton())
+
+// INPUT SHOW 
+
+const searchIcon = document.querySelector('.search')
+const searchInput = document.querySelector('.search.input')
+const sectionCover = document.querySelector('section.cover')
+
+const showingInput = () => {
+
+    searchIcon.addEventListener('click', () => {
+        searchIcon.classList.add('hidden');
+        searchInput.classList.add('show-input')
+        document.querySelector('header.fixed').classList.add('fadeBlack')
+        sectionCover.classList.add('hidden')
+    })
+
+    searchInput.querySelector('i').addEventListener('click', () => {
+        searchIcon.classList.remove('hidden');
+        searchInput.classList.remove('show-input')
+        document.querySelector('header.fixed').classList.remove('fadeBlack')
+        sectionCover.classList.remove('hidden')
+    })
+
+}
+
+showingInput()
+
+// MENU SHOW 
+
+const headerBlack = document.querySelector('header.aside')
+const asideMenu = document.querySelector('aside.menu-genres')
+
+const openMenu = document.querySelector('header.fixed .genres')
+const closeMenu = headerBlack.querySelector('i')
+
+const showingMenu = () => {
+
+    openMenu.addEventListener('click', () => {
+        headerBlack.classList.remove('hidden')
+        asideMenu.classList.remove('hidden')
+       
+    })
+
+    closeMenu.addEventListener('click', () => {
+        headerBlack.classList.add('hidden')
+        asideMenu.classList.add('hidden')
+
+    })
+
+}
+
+showingMenu()
